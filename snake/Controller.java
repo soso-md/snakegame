86128b0e853f872e7a4f7ca24498cb2e0c9391c0
Sophie Raps <s.raps@onveda>
2022-01-17 00:36:48 +0100

    inital commit

---
+++ snake/Controller.java
@@ -0,0 +1,28 @@
+package snake;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class Controller {
+	
+	@FXML
+	private AnchorPane StartAnchorPane;
+
+	public void startGame(ActionEvent e) throws IOException {
+		AnchorPane root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+		StartAnchorPane.getChildren().setAll(root);
+		System.out.println("START GAME");
+	
+	}
+}
---
+++ snake/GameController.java
@@ -0,0 +1,21 @@
+package snake;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class GameController implements Initializable{
+	
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
